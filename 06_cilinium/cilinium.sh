d4e381cb12a6fd02bb679db903a30d2da584981e
Luis Farinango <44455402+LuisFFarinangoT@users.noreply.github.com>
2026-04-15 22:50:55 -0500

    Cilim config updated

--- 06_cilinium/cilinium.sh
+++ 06_cilinium/cilinium.sh
@@ -26,4 +26,25 @@ cilium install \
   --set k8sServicePort=${API_SERVER_PORT} \
   --set ipam.operator.clusterPoolIPv4PodCIDRList=$POD_CIDR \
   --set kubeProxyReplacement=true \
+  --set hubble.relay.enabled=true \
+  --set hubble.ui.enabled=true
   --helm-set=operator.replicas=1
+
+
+helm install cilium cilium/cilium --version 1.14.2 \
+--namespace kube-system \
+--set bgpControlPlane.enabled=true \
+--set tunnel=disabled \
+--set ipam.operator.clusterPoolIPv4PodCIDRList=10.42.0.0/16 \
+--set kubeProxyReplacement=true \
+--set k8sServiceHost=192.168.100.58git \
+--set k8sServicePort=6443 \
+--set routingMode=native \
+--set autoDirectNodeRoutes=true \
+--set ipv4NativeRoutingCIDR=10.42.0.0/16 \
+--set loadBalancer.mode=dsr \
+--set ipv4.enabled=true \
+--set prometheus.enabled=true \
+--set operator.prometheus.enabled=true \
+--set hubble.enabled=true \
+--set hubble.metrics.enabled="{dns,drop,tcp,flow,port-distribution,icmp,http}"
